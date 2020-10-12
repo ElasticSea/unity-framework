b9163c8ade4f5ef5fb51ffdf535c0f8c2b096251
ElasticSea <info@elasticsea.com>
2020-10-12 14:19:29 +0200

    Adding scene list.

---
+++ Core/Scripts/Util/SceneList.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace Core.Util
+{
+    public class SceneList : EditorWindow
+    {
+        private IEnumerable<EditorBuildSettingsScene> scenes = Enumerable.Empty<EditorBuildSettingsScene>();
+
+        [MenuItem("Window/Scene List")]
+        public static void ShowWindow() => GetWindow(typeof(SceneList), false, "Scenes");
+
+        private void Update()
+        {
+            var newScenes = EditorBuildSettings.scenes.ToList();
+            if (scenes.SequenceEqual(newScenes) == false)
+            {
+                scenes = EditorBuildSettings.scenes.ToList();
+                Repaint();
+            }
+        }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.BeginVertical();
+
+            foreach (var scene in EditorBuildSettings.scenes)
+            {
+                var name = Path.GetFileNameWithoutExtension(scene.path);
+
+                GUI.backgroundColor = scene.enabled ? new Color(0.9f, 0.9f, 0.9f) : new Color(0.4f, 0.4f, 0.4f);
+                if (GUILayout.Button(name))
+                {
+                    EditorSceneManager.OpenScene(scene.path);
+                }
+            }
+            EditorGUILayout.EndVertical();
+        }
+    }
+}
