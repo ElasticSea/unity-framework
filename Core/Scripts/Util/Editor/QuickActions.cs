b85abab05ab08a637c7cdb439da8bad29d4ca65c
ElasticSea <info@elasticsea.com>
2020-10-18 19:17:11 +0200

    Adding quick actions.

---
+++ Core/Scripts/Util/Editor/QuickActions.cs
@@ -0,0 +1,21 @@
+using Core.Extensions;
+using Core.Util;
+using UnityEditor;
+using UnityEngine;
+
+namespace Core.Util
+{
+    public class QuickActions : EditorWindow
+    {
+        [MenuItem("Window/Quick Actions")]
+        public static void ShowWindow() => GetWindow(typeof(QuickActions), false, "Quick Actions");
+
+        private void OnGUI()
+        {
+            if (GUILayout.Button("Show Persistent Folder"))
+            {
+                Utils.OpenInExplorer(Application.persistentDataPath);
+            }
+        }
+    }
+}
