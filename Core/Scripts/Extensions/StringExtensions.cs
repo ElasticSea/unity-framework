2939e1fb29c7b9d232d88c3ed79aca91c0d62fba
ElasticSea <info@elasticsea.com>
2020-11-06 04:21:33 +0100

    Adding remove at end extensions.

--- Core/Scripts/Extensions/StringExtensions.cs
+++ Core/Scripts/Extensions/StringExtensions.cs
@@ -325,5 +325,17 @@ namespace Core.Extensions
 
 			return output.ToString();
 		}
+		
+		public static string RemoveFromEnd(this string s, string suffix)
+		{
+			if (s.EndsWith(suffix))
+			{
+				return s.Substring(0, s.Length - suffix.Length);
+			}
+			else
+			{
+				return s;
+			}
+		}
     }
 }
