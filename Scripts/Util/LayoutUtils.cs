c32a60d6f9ef14b02bed47fcc4c9cb503c5ffaf8
ElasticSea <info@elasticsea.com>
2021-03-12 20:18:36 +0100

    Adding layout utils

---
+++ Scripts/Util/LayoutUtils.cs
@@ -0,0 +1,17 @@
+using UnityEditor;
+using UnityEngine;
+
+#if UNITY_EDITOR
+namespace ElasticSea.Framework.Util
+{
+    public static class LayoutUtils
+    {
+        public static void Line(int height = 1, Color? color = null)
+        {
+            var rect = EditorGUILayout.GetControlRect(false, height);
+            rect.height = height;
+            EditorGUI.DrawRect(rect, color ?? new Color(0.5f, 0.5f, 0.5f, 1));
+        }
+    }
+}
+#endif
