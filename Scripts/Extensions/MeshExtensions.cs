a2f1bed477daef7f95614d5b267106c2f10df168
ElasticSea <info@elasticsea.com>
2021-10-27 00:11:52 +0200

    Offset mesh from center

--- Scripts/Extensions/MeshExtensions.cs
+++ Scripts/Extensions/MeshExtensions.cs
@@ -30,5 +30,34 @@ namespace ElasticSea.Framework.Extensions
             var v123 = p1.x * p2.y * p3.z;
             return (1.0f / 6.0f) * (-v321 + v231 + v312 - v132 - v213 + v123);
         }
+        
+        /// <summary>
+        /// Moves vertices from the center to each direction
+        /// </summary>
+        public static Mesh OffsetMeshFromCenter(this Mesh mesh, Vector3 size)
+        {
+            var bounds = mesh.bounds;
+            var vertices = mesh.vertices;
+            var offset = (size - bounds.size) / 2;
+            for (var i = 0; i < vertices.Length; i++)
+            {
+                var vert = vertices[i];
+                var x = vert.x > bounds.center.x / 2 ? vert.x + offset.x : vert.x - offset.x;
+                var y = vert.y > bounds.center.y / 2 ? vert.y + offset.y : vert.y - offset.y;
+                var z = vert.z > bounds.center.z / 2 ? vert.z + offset.z : vert.z - offset.z;
+                vertices[i] = new Vector3(x, y, z);
+            }
+        
+            var newMesh = new Mesh
+            {
+                vertices = vertices,
+                triangles = mesh.triangles,
+                normals = mesh.normals,
+                uv = mesh.uv,
+                tangents = mesh.tangents
+            };
+            newMesh.RecalculateBounds();
+            return newMesh;
+        }
     }
 }
