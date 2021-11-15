1043f4e92f874297498ed6fd2a042f331d2dec62
ElasticSea <info@elasticsea.com>
2021-11-15 05:32:17 +0100

    Adding ToEnumerable extension for Enumerator

--- Scripts/Extensions/GenericEnumerableExtensions.cs
+++ Scripts/Extensions/GenericEnumerableExtensions.cs
@@ -1044,5 +1044,13 @@ namespace ElasticSea.Framework.Extensions
 
 		    return maxElem;
 	    }
+
+	    public static IEnumerable<T> ToEnumerable<T>(this IEnumerator<T> enumerator)
+	    {
+		    while (enumerator.MoveNext())
+		    {
+			    yield return enumerator.Current;
+		    }
+	    }
     }
 }
