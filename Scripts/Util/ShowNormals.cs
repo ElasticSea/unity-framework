76c50d5c5e80fdcc1f5234b0c33dfa69aa56bf49
Elastic Sea <info@elasticsea.com>
2022-07-29 21:26:41 +0200

    Adding offset to show normals

--- Scripts/Util/ShowNormals.cs
+++ Scripts/Util/ShowNormals.cs
@@ -6,6 +6,7 @@ namespace ElasticSea.Framework.Util
     {
         [SerializeField] private float length = 1;
         [SerializeField] private int limit = -1;
+        [SerializeField] private int offset = 0;
         
         [SerializeField] private MeshFilter mf;
         [SerializeField] private SkinnedMeshRenderer smr;
@@ -39,7 +40,7 @@ namespace ElasticSea.Framework.Util
 
                 var len = limit >= 0 ? Mathf.Min(limit, normals.Length) : normals.Length;
 
-                for (var i = 0; i < len; i++)
+                for (var i = offset; i < offset + len; i++)
                 {
                     Gizmos.color = Color.Lerp(startColor, endColor, (float) i / (normals.Length - 1));
 
