5664d87529a7c2bb8bf89f336fd407fca0e1c865
Elastic Sea <info@elasticsea.com>
2022-08-04 21:21:09 +0200

    Removing json.net dependency

--- Scripts/Util/Icons/GoogleIconPack.cs
+++ Scripts/Util/Icons/GoogleIconPack.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using Newtonsoft.Json;
 using UnityEngine;
 
 namespace ElasticSea.Framework.Scripts.Util.Icons
@@ -26,8 +25,8 @@ namespace ElasticSea.Framework.Scripts.Util.Icons
         
         [SerializeField] private TextAsset config;
 
-        public override Dictionary<string, int> Icons => JsonConvert
-            .DeserializeObject<Config>(config.text)
+        public override Dictionary<string, int> Icons => JsonUtility
+            .FromJson<Config>(config.text)
             .categories.SelectMany(c => c.icons)
             .ToDictionary(g => g.name, g => int.Parse(g.codepoint, NumberStyles.HexNumber));
     }
