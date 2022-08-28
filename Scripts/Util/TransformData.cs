5f94bfafecd0c4489901903e76b56d0fb7e0fe08
Elastic Sea <info@elasticsea.com>
2022-08-28 15:24:38 +0200

    Adding useful transform data object

---
+++ Scripts/Util/TransformData.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+namespace ElasticSea.Framework.Util
+{
+    [Serializable]
+    public class TransformData
+    {
+        public Vector3 Position;
+        public Vector3 Rotation;
+        public Vector3 Scale;
+    }
+}
