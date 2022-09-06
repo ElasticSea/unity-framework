7835a8c12d2b6bd00122a8e6d421314124fb2fb5
Elastic Sea <info@elasticsea.com>
2022-09-06 15:08:37 +0200

    Draw capsule

--- Scripts/Util/GizmoUtils.cs
+++ Scripts/Util/GizmoUtils.cs
@@ -28,5 +28,45 @@ namespace ElasticSea.Framework.Util
             Handles.DrawBezier(from, to, from, to, Gizmos.color, null, thickness);
 #endif
         }
+
+        public static void DrawWireCapsule(Vector3 p1, Vector3 p2, float radius)
+        {
+#if UNITY_EDITOR
+            // Special case when both points are in the same position
+            if (p1 == p2)
+            {
+                // DrawWireSphere works only in gizmo methods
+                Gizmos.DrawWireSphere(p1, radius);
+                return;
+            }
+
+            using (new Handles.DrawingScope(Gizmos.color, Gizmos.matrix))
+            {
+                Quaternion p1Rotation = Quaternion.LookRotation(p1 - p2);
+                Quaternion p2Rotation = Quaternion.LookRotation(p2 - p1);
+                // Check if capsule direction is collinear to Vector.up
+                float c = Vector3.Dot((p1 - p2).normalized, Vector3.up);
+                if (c == 1f || c == -1f)
+                {
+                    // Fix rotation
+                    p2Rotation = Quaternion.Euler(p2Rotation.eulerAngles.x, p2Rotation.eulerAngles.y + 180f, p2Rotation.eulerAngles.z);
+                }
+
+                // First side
+                Handles.DrawWireArc(p1, p1Rotation * Vector3.left, p1Rotation * Vector3.down, 180f, radius);
+                Handles.DrawWireArc(p1, p1Rotation * Vector3.up, p1Rotation * Vector3.left, 180f, radius);
+                Handles.DrawWireDisc(p1, (p2 - p1).normalized, radius);
+                // Second side
+                Handles.DrawWireArc(p2, p2Rotation * Vector3.left, p2Rotation * Vector3.down, 180f, radius);
+                Handles.DrawWireArc(p2, p2Rotation * Vector3.up, p2Rotation * Vector3.left, 180f, radius);
+                Handles.DrawWireDisc(p2, (p1 - p2).normalized, radius);
+                // Lines
+                Handles.DrawLine(p1 + p1Rotation * Vector3.down * radius, p2 + p2Rotation * Vector3.down * radius);
+                Handles.DrawLine(p1 + p1Rotation * Vector3.left * radius, p2 + p2Rotation * Vector3.right * radius);
+                Handles.DrawLine(p1 + p1Rotation * Vector3.up * radius, p2 + p2Rotation * Vector3.up * radius);
+                Handles.DrawLine(p1 + p1Rotation * Vector3.right * radius, p2 + p2Rotation * Vector3.left * radius);
+            }
+#endif
+        }
     }
 }
