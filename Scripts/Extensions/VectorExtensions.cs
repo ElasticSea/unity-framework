2acb75e2c643f8cb0445614043746c35e046e81c
Elastic Sea <info@elasticsea.com>
2022-09-08 13:01:42 +0200

    Adding clamp methods

--- Scripts/Extensions/VectorExtensions.cs
+++ Scripts/Extensions/VectorExtensions.cs
@@ -128,6 +128,24 @@ namespace ElasticSea.Framework.Extensions
 			return new Vector3(x, y, z);
 		}
 
+		public static Vector3 ClampX(this Vector3 vector, float min, float max)
+		{
+			var x = Mathf.Clamp(vector.x, min, max);
+			return new Vector3(x, vector.y, vector.z);
+		}
+
+		public static Vector3 ClampY(this Vector3 vector, float min, float max)
+		{
+			var y = Mathf.Clamp(vector.y, min, max);
+			return new Vector3(vector.x, y, vector.z);
+		}
+
+		public static Vector3 ClampZ(this Vector3 vector, float min, float max)
+		{
+			var z = Mathf.Clamp(vector.z, min, max);
+			return new Vector3(vector.x, vector.y, z);
+		}
+
 		public static Vector3 Clamp(this Vector3 vector, Vector3 min, Vector3 max)
 		{
 			var x = Mathf.Clamp(vector.x, min.x, max.x);
