2ba834e21d37c19baf3f49403b1cd7bc281667b2
Elastic Sea <info@elasticsea.com>
2022-10-29 11:01:21 +0200

    Overload for mipmaps

--- Scripts/Extensions/RenderTextureExtensions.cs
+++ Scripts/Extensions/RenderTextureExtensions.cs
@@ -5,11 +5,11 @@ namespace ElasticSea.Framework.Scripts.Extensions
 {
     public static class RenderTextureExtensions
     {
-        public static Texture2D ToTexture2D(this RenderTexture renderTexture, TextureFormat textureFormat)
+        public static Texture2D ToTexture2D(this RenderTexture renderTexture, TextureFormat textureFormat = TextureFormat.ARGB32, bool mipmaps = true)
         {
             var activeRenderTexture = RenderTexture.active;
             
-            var texture = new Texture2D(renderTexture.width, renderTexture.height, textureFormat, renderTexture.mipmapCount, false);
+            var texture = new Texture2D(renderTexture.width, renderTexture.height, textureFormat, mipmaps);
             RenderTexture.active = renderTexture;
             texture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
             texture.Apply();
