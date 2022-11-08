c29616c6107fb538e18fec8c6c3209edb6779330
Elastic Sea <info@elasticsea.com>
2022-11-08 12:53:08 +0100

    Convert pixel array with multithreading

--- Scripts/Extensions/Texture2DExtensions.cs
+++ Scripts/Extensions/Texture2DExtensions.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace ElasticSea.Framework.Scripts.Extensions
@@ -23,6 +26,43 @@ namespace ElasticSea.Framework.Scripts.Extensions
             }
 
             return new Color(r / length, g / length, b / length, a / length);
+        }
+        
+        public delegate Color GetPixel(int x, int y);
+        public delegate Color Build(GetPixel getPixel, int x, int y);
+        public static Color[] ConvertMt(this Color[] pixels, int width, int height, Build build)
+        {
+            var output = new Color[width * height];
+            
+            var chunkSize = width * height / (SystemInfo.processorCount * 8);
+
+            var workPackages = new List<(int start, int end)>();
+
+            var sizeBudget = output.Length;
+            var i = 0;
+            while (sizeBudget > 0)
+            {
+                var currentChunk = Mathf.Min(chunkSize, sizeBudget);
+
+                workPackages.Add((i, i + currentChunk));
+                sizeBudget -= currentChunk;
+                i += currentChunk;
+            }
+
+            GetPixel getPixel = (x, y) => pixels[y * width + x];
+
+            Parallel.ForEach(workPackages, tuple =>
+            {
+                var (start, end) = tuple;
+                for (var j = start; j < end; j++)
+                {
+                    var x = j % width;
+                    var y = j / width;
+                    output[j] = build(getPixel, x, y);
+                }
+            });
+
+            return output;
         }
     }
 }
