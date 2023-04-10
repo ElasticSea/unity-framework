34f131684e152ffd60cc4b627d75aecfbbd5d635
Elastic Sea <info@elasticsea.com>
2023-04-10 12:14:26 +0200

    Adding path extensions

---
+++ Scripts/Extensions/PathExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace ElasticSea.Framework.Scripts.Extensions
+{
+    public static class PathExtensions
+    {
+        public static void EnsureDirectory(this FileInfo fileInfo)
+        {
+            fileInfo.Directory.EnsureDirectory();
+        }
+        
+        public static void EnsureDirectory(this DirectoryInfo directoryInfo)
+        {
+            if (directoryInfo.Exists == false)
+            {
+                directoryInfo.Create();
+            }
+        }
+    }
+}
