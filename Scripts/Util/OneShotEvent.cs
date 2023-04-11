de94c8db774805003c063895dbf2c1a7ffc74d9e
Elastic Sea <info@elasticsea.com>
2023-04-11 11:59:47 +0200

    Adding one shot event

---
+++ Scripts/Util/OneShotEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Blocks;
+
+namespace ElasticSea.Framework.Scripts.Util
+{
+    public class OneShotEvent<T>
+    {
+        private event Action<T> internalEvent;
+
+        public event Action<T> SomeEvent = obj => { };
+
+        public OneShotEvent()
+        {
+            internalEvent += obj =>
+            {
+                foreach (Action<T> del in SomeEvent.GetInvocationList())
+                {
+                    del.DynamicInvoke(obj);
+                    SomeEvent -= del;
+                }
+            };
+        }
+
+        public void Invoke(T value)
+        {
+            internalEvent.Invoke(value);
+        }
+    }
+}
