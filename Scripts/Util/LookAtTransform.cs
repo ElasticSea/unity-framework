7516f10ec022caf0d71c54374e390d555baea11c
Elastic Sea <info@elasticsea.com>
2023-05-03 06:38:03 +0200

    Couple transform scripts

---
+++ Scripts/Util/LookAtTransform.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace ElasticSea.Framework.Scripts.Util
+{
+    public class LookAtTransform : MonoBehaviour
+    {
+        public Transform target;
+
+        public Transform Target
+        {
+            get => target;
+            set
+            {
+                target = value;
+                Update();
+            }
+        }
+
+        private void Update()
+        {
+            transform.rotation = Quaternion.LookRotation(target.forward, target.up);
+        }
+    }
+}
---
+++ Scripts/Util/SynchronizePosition.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace ElasticSea.Framework.Scripts.Util
+{
+    public class SynchronizePosition : MonoBehaviour
+    {
+        public Transform target;
+        public Vector3 offset;
+
+        public Vector3 Offset
+        {
+            get => offset;
+            set
+            {
+                offset = value;
+                if (target)
+                {
+                    Update();
+                }
+            }
+        }
+
+        public Transform Target
+        {
+            get => target;
+            set
+            {
+                target = value;
+                Update();
+            }
+        }
+
+        private void Update()
+        {
+            transform.position = target.position + offset;
+        }
+    }
+}
