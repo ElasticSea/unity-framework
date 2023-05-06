80fdd57802c2a8a7c4244fe8a82b1515145b7b74
Elastic Sea <info@elasticsea.com>
2023-05-06 06:17:51 +0200

    Dont destroy on load

---
+++ Scripts/Util/DontDestroyOnLoad.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace ElasticSea.Framework.Scripts.Util
+{
+    public class DontDestroyOnLoad : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
