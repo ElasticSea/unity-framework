13cab46f513816d32b86840976ed48adb43001a7
Elastic Sea <developer@elasticsea.com>
2023-12-11 14:51:56 +0100

    Pause editor after some time

---
+++ Scripts/Util/PauseEditorAfterTime.cs
@@ -0,0 +1,28 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace ElasticSea.Framework.Util
+{
+    public class PauseEditorAfterTime : MonoBehaviour
+    {
+        [SerializeField] private float time;
+
+        private bool paused;
+
+        private void Update()
+        {
+            if (paused == false)
+            {
+                if (Time.time > time)
+                {
+#if UNITY_EDITOR
+                    EditorApplication.isPaused = true;
+#endif
+                    paused = true;
+                }
+            }
+        }
+    }
+}
