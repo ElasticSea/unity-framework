16006639d096fe24d27466453fab570d88f5a359
Elastic Sea <developer@elasticsea.com>
2024-10-21 01:53:25 +0200

    New mesh utility

---
+++ Scripts/Util/MeshUtilityV2.cs
@@ -0,0 +1,37 @@
+﻿using ElasticSea.Framework.Extensions;
+using UnityEngine;
+
+namespace ElasticSea.Framework.Util
+{
+    public class MeshUtilityV2
+    {
+        private readonly Mesh source;
+        private readonly Mesh finalMesh;
+        private readonly Vector3[] originalVertices;
+        private readonly Bounds originalBounds;
+        private readonly Vector3[] newVertices;
+
+        public Mesh Mesh => finalMesh;
+        public Mesh Source => source;
+        public Bounds Bounds => originalBounds;
+
+        public MeshUtilityV2(Mesh source)
+        {
+            this.source = source;
+            this.originalVertices = source.vertices;
+            this.originalBounds = source.bounds;
+            this.finalMesh = source.Clone();
+            this.newVertices = new Vector3[source.vertices.Length];
+        }
+
+        public delegate void UpdateVerticesHandler(Vector3[] originalVertices, Vector3[] newVertices);
+
+        public void UpdateVertices(UpdateVerticesHandler factory)
+        {
+            factory(originalVertices, newVertices);
+
+            finalMesh.vertices = newVertices;
+            finalMesh.RecalculateBounds();
+        }
+    }
+}
