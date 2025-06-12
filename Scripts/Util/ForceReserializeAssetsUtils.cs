c040bc3d8fb5b71a27466295c84932cdb5ba6ff5
Elastic Sea <developer@elasticsea.com>
2025-06-12 11:40:37 +0200

    ForceReserializeAssetsUtils

---
+++ Scripts/Util/ForceReserializeAssetsUtils.cs
@@ -0,0 +1,17 @@
+﻿#if UNITY_EDITOR
+using System.Linq;
+using UnityEditor;
+
+namespace ElasticSea.Framework.Util
+{
+    public class ForceReserializeAssetsUtils
+    {
+        [MenuItem("Assets/Force Reserialize Assets")]
+        public static void ForceReserializeAssets()
+        {
+            var paths = Selection.assetGUIDs.Select(guid => AssetDatabase.GUIDToAssetPath(guid));
+            AssetDatabase.ForceReserializeAssets(paths);
+        }
+    }
+}
+#endif
