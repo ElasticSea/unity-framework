c079ddd66df3838f9c2cd1908904fa0882c2cc6f
elasticsea <developer@elasticsea.com>
2025-06-25 17:48:23 +0200

    Adding bounding box script

---
+++ Scripts/Util/BoundingBox.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace ElasticSea.Framework.Util
+{
+    public class BoundingBox : MonoBehaviour
+    {
+        public Vector3 Center = Vector3.zero;
+        [Min(0f)] public Vector3 Size = Vector3.one;
+
+        [Header("Gizmos")]
+        [SerializeField] Color color = Color.yellow;
+        [SerializeField] bool drawWhenNotSelected = false;
+
+        private void OnDrawGizmos()
+        {
+            if (drawWhenNotSelected) DrawGizmo();
+        }
+        
+        private void OnDrawGizmosSelected()
+        {
+            if (!drawWhenNotSelected) DrawGizmo();
+        }
+
+        public Bounds LocalBounds
+        {
+            get => new(Center, Size);
+            set
+            {
+                Size = value.size;
+                Center = value.center;
+            }
+        }
+
+        public Bounds WorldBounds
+        {
+            get
+            {
+                var worldCenter = transform.TransformPoint(Center);
+                var worldSize = Vector3.Scale(Size, transform.lossyScale);
+                return new Bounds(worldCenter, worldSize);
+            }
+        }
+
+        private void DrawGizmo()
+        {
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.color = color;
+            Gizmos.DrawWireCube(Center, Size);
+        }
+    }
+}
