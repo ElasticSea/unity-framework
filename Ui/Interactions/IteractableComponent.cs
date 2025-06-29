0c15ccf1c75f7faf5049f1c239cc4d72e13af337
elasticsea <developer@elasticsea.com>
2025-06-29 22:35:55 +0200

    Adding IteractableComponent

---
+++ Ui/Interactions/IteractableComponent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace ElasticSea.Framework.Ui.Interactions
+{
+    public class IteractableComponent : MonoBehaviour, IInteractable
+    {
+        public IInteractable Interactable;
+        public bool IsActive = true;
+        
+        // Used for pinch interactions mainly on vision os
+        public bool CancelByMove = false;
+
+        public void Hover(InteractionEvent interactionEvent) => Interactable.Hover(interactionEvent);
+        public void UnHover(InteractionEvent interactionEvent) => Interactable.UnHover(interactionEvent);
+        public void Press(InteractionEvent interactionEvent) => Interactable.Press(interactionEvent);
+        public void Release(InteractionEvent interactionEvent) => Interactable.Release(interactionEvent);
+        public void Cancel(InteractionEvent interactionEvent) => Interactable.Cancel(interactionEvent);
+        public void Move(InteractionEvent interactionEvent) => Interactable.Move(interactionEvent);
+    }
+}
