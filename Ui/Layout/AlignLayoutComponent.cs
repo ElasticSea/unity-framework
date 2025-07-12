f51d030e99ad4559d143ed22169bc64d67145314
elasticsea <developer@elasticsea.com>
2025-07-13 01:17:10 +0200

    AlignLayoutComponent

---
+++ Ui/Layout/AlignLayoutComponent.cs
@@ -0,0 +1,37 @@
+﻿using ElasticSea.Framework.Ui.Layout.Alignment;
+using ElasticSea.Framework.Util.PropertyDrawers;
+using UnityEngine;
+
+namespace ElasticSea.Framework.Ui.Layout
+{
+    public class AlignLayoutComponent : MonoBehaviour
+    {
+        [CustomObjectPicker(typeof(ILayoutComponent))] [SerializeField]
+        private Component _layoutComponent;
+
+        private ILayoutComponent layoutComponent => (ILayoutComponent)_layoutComponent;
+        [SerializeField] private Align horizontal = Align.Center;
+        [SerializeField] private Align vertical = Align.Center;
+
+        private void Awake()
+        {
+            layoutComponent.OnRectChanged += () => AlignLayout();
+            AlignLayout();
+        }
+
+        private void AlignLayout()
+        {
+            var rect = layoutComponent.Rect;
+            var horizontalDelta = horizontal.GetAlignDelta();
+            var verticalDelta = vertical.GetAlignDelta();
+            var x = -Mathf.Lerp(rect.xMin, rect.xMax, horizontalDelta);
+            var y = -Mathf.Lerp(rect.yMin, rect.yMax, verticalDelta);
+            transform.localPosition = new Vector3(x, y, 0);
+        }
+
+        private void OnValidate()
+        {
+            AlignLayout();
+        }
+    }
+}
