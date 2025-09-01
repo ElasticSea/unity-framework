1bbb0185680b2055b0cdf5d72f16b43167732fc8
elasticsea <developer@elasticsea.com>
2025-09-02 01:28:53 +0200

    New mesh builder

---
+++ Scripts/Util/IntermediateMesh.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using ElasticSea.Framework.Extensions;
+using UnityEngine;
+
+namespace ElasticSea.Framework.Util
+{
+    public class IntermediateMesh
+    {
+        public readonly Mesh Mesh;
+        public readonly Vector3[] OriginalVertices;
+        public readonly Bounds OriginalBounds;
+        public readonly int Count;
+        
+        private readonly Vector3[] vertices;
+        private Bounds bounds;
+
+        public IntermediateMesh(Mesh originalMesh)
+        {
+            Mesh = originalMesh.Clone();
+            OriginalVertices = Mesh.vertices;
+            OriginalBounds = Mesh.bounds;
+            Count = OriginalVertices.Length;
+            
+            vertices = new Vector3[Count];
+            Array.Copy(OriginalVertices, vertices, Count);
+            bounds = OriginalBounds;
+        }
+
+        public void Apply()
+        {
+            Mesh.vertices = vertices;
+            Mesh.bounds = bounds;
+        }
+
+        public void Reset()
+        {
+            Array.Copy(OriginalVertices, vertices, Count);
+            bounds = OriginalBounds;
+        }
+
+        public void Scale(Vector3 scale)
+        {
+            for (var i = 0; i < Count; i++)
+            {
+                var vertex = vertices[i];
+                vertex.Scale(scale);
+                vertices[i] = vertex;
+            }
+
+            bounds = bounds.Scale(scale);
+        }
+
+        public void ExtendToBounds(Bounds boundsToMatch)
+        {
+            var boundsCenter = bounds.center;
+
+            var minExtends = boundsToMatch.min - bounds.min;
+            var maxExtends = boundsToMatch.max - bounds.max;
+
+            for (var i = 0; i < vertices.Length; i++)
+            {
+                var vert = vertices[i];
+                var x = vert.x + (vert.x > boundsCenter.x ? maxExtends.x : minExtends.x);
+                var y = vert.y + (vert.y > boundsCenter.y ? maxExtends.y : minExtends.y);
+                var z = vert.z + (vert.z > boundsCenter.z ? maxExtends.z : minExtends.z);
+                vertices[i] = new Vector3(x, y, z) ;
+            }
+
+            bounds = boundsToMatch;
+        }
+    }
+}
---
+++ Scripts/Util/IntermediateMeshExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace ElasticSea.Framework.Util
+{
+    public static class IntermediateMeshExtensions
+    {
+        public static IntermediateMesh Create(this Mesh mesh)
+        {
+            var intermediateMesh = new IntermediateMesh(mesh);
+            return intermediateMesh;
+        }
+    }
+}
