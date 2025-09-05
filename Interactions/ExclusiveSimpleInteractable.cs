7f2aff8edb742621e8720adad97b04865c8fe5e5
elasticsea <developer@elasticsea.com>
2025-09-05 03:46:07 +0200

    Interaction with a lock

---
+++ Interactions/ExclusiveSimpleInteractable.cs
@@ -0,0 +1,51 @@
+﻿namespace ElasticSea.Framework.Interactions
+{
+    public class ExclusiveSimpleInteractable : IInteractable
+    {
+        private readonly IInteractable interactable;
+        private object handLock;
+
+        public ExclusiveSimpleInteractable(IInteractable interactable)
+        {
+            this.interactable = interactable;
+        }
+
+        public void Hover(InteractionEvent evnt) => interactable?.Hover(evnt);
+        public void UnHover(InteractionEvent evnt) => interactable?.UnHover(evnt);
+
+        public void Press(InteractionEvent evnt)
+        {
+            if (handLock == null)
+            {
+                handLock = evnt.InteractionProvider;
+                interactable?.Press(evnt);
+            }
+        }
+
+        public void Release(InteractionEvent evnt)
+        {
+            if (handLock == evnt.InteractionProvider)
+            {
+                handLock = null;
+                interactable?.Release(evnt);
+            }
+        }
+
+        public void Cancel(InteractionEvent evnt)
+        {
+            if (handLock == evnt.InteractionProvider)
+            {
+                handLock = null;
+                interactable?.Cancel(evnt);
+            }
+        }
+
+        public void Move(InteractionEvent evnt)
+        {
+            if (handLock == evnt.InteractionProvider)
+            {
+                interactable?.Move(evnt);
+            }
+        }
+    }
+}
---
+++ Interactions/InteractionsExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace ElasticSea.Framework.Interactions
+{
+    public static class InteractionsExtensions
+    {
+        public static ExclusiveSimpleInteractable AsExclusive(this IInteractable interactions)
+        {
+            return new ExclusiveSimpleInteractable(interactions);
+        }
+    }
+}
