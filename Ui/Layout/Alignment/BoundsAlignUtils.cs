173be3b62744a9a2d7cc7543b358e955e8c75dfa
elasticsea <developer@elasticsea.com>
2025-12-01 02:30:25 +0100

    Adding back bounds

--- Ui/Layout/Alignment/BoundsAlignUtils.cs
+++ Ui/Layout/Alignment/BoundsAlignUtils.cs
@@ -68,5 +68,71 @@ namespace ElasticSea.Framework.Ui.Layout.Alignment
             var worldPosition = target.MultiplyPoint(localPosition);
             return (worldPosition, finalScale);
         }
+        
+        public static (Vector3 position, Quaternion rotation, Vector3 scale) Align(Quaternion sourceRotation, Bounds sourceBounds, Transform target, Bounds targetBounds, Align horizontal, Align vertical, Align depth, bool canScale = true, bool canRotate = true, Align oversizedHorizontal = Center, Align oversizedVertical = Center, Align oversizedDepth = Center, bool rotationIsWorld = true, Quaternion? gejge = null)
+        {
+            var trs = Matrix4x4.TRS(target.position, target.rotation, target.localScale);
+            return Align(sourceRotation, sourceBounds, trs, targetBounds, horizontal, vertical, depth, canScale, canRotate, oversizedHorizontal, oversizedVertical, oversizedDepth, rotationIsWorld, gejge);
+        }
+        
+        public static (Vector3 position, Quaternion rotation, Vector3 scale) Align(Quaternion sourceRotation, Bounds sourceBounds, Matrix4x4 target, Bounds targetBounds, Align horizontal, Align vertical, Align depth, bool canScale = true, bool canRotate = true, Align oversizedHorizontal = Center, Align oversizedVertical = Center, Align oversizedDepth = Center, bool rotationIsWorld = true, Quaternion? gejge = null)
+        {
+            // Rotation
+            var snappedLocalRotation = Quaternion.identity;
+            if (canRotate)
+            {
+                var localRotation = rotationIsWorld ? Quaternion.Inverse(target.rotation) * sourceRotation : sourceRotation;
+                snappedLocalRotation = Quaternion.Euler((localRotation.eulerAngles + new Vector3(360, 360, 360)).Snap(90));
+            }
+            
+            // Bounds
+            var rotationAdjsutedBounds = sourceBounds;
+            rotationAdjsutedBounds.center = snappedLocalRotation * rotationAdjsutedBounds.center;
+            rotationAdjsutedBounds.size = (snappedLocalRotation * rotationAdjsutedBounds.size).Abs();
+
+            // Scale
+            var worldScale = Vector3.one;
+            if (canScale)
+            {
+                var scaleFactor = targetBounds.size.Divide(rotationAdjsutedBounds.size);
+                var minScale = scaleFactor.Min();
+                worldScale *= minScale;
+            }
+            else
+            {
+                if (rotationAdjsutedBounds.size.x > targetBounds.size.x) horizontal = oversizedHorizontal;
+                if (rotationAdjsutedBounds.size.y > targetBounds.size.y) vertical = oversizedVertical;
+                if (rotationAdjsutedBounds.size.z > targetBounds.size.z) depth = oversizedDepth;
+            }
+
+            // Bounds
+            var scaledBounds = new Bounds(rotationAdjsutedBounds.center.Multiply(worldScale), rotationAdjsutedBounds.size.Multiply(worldScale));
+            var boundsSizeDifference = targetBounds.size - scaledBounds.size;
+
+            float GetAlignment(Align alignment, float sourceMin, float targetMin, float sizeDifference)
+            {
+                switch (alignment)
+                {
+                    case Start:
+                        return targetMin - sourceMin;
+                    case Center:
+                        return targetMin - sourceMin + sizeDifference / 2;
+                    case End:
+                        return targetMin - sourceMin + sizeDifference;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(horizontal), horizontal, null);
+                }
+            }
+
+            // Position
+            var localPosition = new Vector3();
+            localPosition.x = GetAlignment(horizontal, scaledBounds.min.x, targetBounds.min.x, boundsSizeDifference.x);
+            localPosition.y = GetAlignment(vertical, scaledBounds.min.y, targetBounds.min.y, boundsSizeDifference.y);
+            localPosition.z = GetAlignment(depth, scaledBounds.min.z, targetBounds.min.z, boundsSizeDifference.z);
+
+            var worldPosition = target.MultiplyPoint(localPosition);
+            var worldRotation = target.rotation * snappedLocalRotation;
+            return (worldPosition, worldRotation, worldScale);
+        }
     }
 }
