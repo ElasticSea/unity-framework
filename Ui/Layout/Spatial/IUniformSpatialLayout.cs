a62cd3ede28325ecc53cce14d26de7e49d013704
elasticsea <developer@elasticsea.com>
2025-12-31 20:06:09 +0100

    Set is not used in any implementatino of uniform spatial layout

--- Ui/Layout/Spatial/IUniformSpatialLayout.cs
+++ Ui/Layout/Spatial/IUniformSpatialLayout.cs
@@ -4,6 +4,6 @@ namespace ElasticSea.Framework.Ui.Layout.Spatial
 {
     public interface IUniformSpatialLayout : ISpatialLayout
     {
-        Vector3 Size { get; set; }
+        Vector3 Size { get; }
     }
 }
