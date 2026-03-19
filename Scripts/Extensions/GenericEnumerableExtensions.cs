d06aaa1db782e0944365dabbe0f5d465a227aea3
elasticsea <developer@elasticsea.com>
2026-03-19 02:30:50 +0100

    Adding GetOrAdd for dict

--- Scripts/Extensions/GenericEnumerableExtensions.cs
+++ Scripts/Extensions/GenericEnumerableExtensions.cs
@@ -1298,5 +1298,18 @@ namespace ElasticSea.Framework.Extensions
 
 		    return result;
 	    }
+	    
+	    public static TValue GetOrAdd<TKey, TValue>(
+		    this IDictionary<TKey, TValue> dictionary, 
+		    TKey key, 
+		    Func<TKey, TValue> valueFactory)
+	    {
+		    if (!dictionary.TryGetValue(key, out var value))
+		    {
+			    value = valueFactory(key);
+			    dictionary[key] = value;
+		    }
+		    return value;
+	    }
 	}
 }
