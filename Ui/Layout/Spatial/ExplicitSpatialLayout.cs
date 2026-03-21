2d2f7bbc8feae9e26cd3a7f6df3ab475cfb31dbe
elasticsea <developer@elasticsea.com>
2026-03-21 06:01:10 +0100

    Adding few layout classes

---
+++ Ui/Layout/Spatial/ExplicitSpatialLayout.cs
@@ -0,0 +1,18 @@
+﻿using ElasticSea.Framework.Extensions;
+using UnityEngine;
+
+namespace ElasticSea.Framework.Ui.Layout.Spatial
+{
+    public class ExplicitSpatialLayout : MonoBehaviour, ISpatialLayout
+    {
+        [SerializeField] private Bounds[] bounds;
+
+        public int Count => bounds.Length;
+        public Bounds Bounds => bounds.Encapsulate();
+
+        public SpatialCell GetCell(int index)
+        {
+            return new SpatialCell(Matrix4x4.identity, bounds[index]);
+        }
+    }
+}
---
+++ Ui/Layout/Spatial/SpatialLayoutGizmos.cs
@@ -0,0 +1,50 @@
+﻿using ElasticSea.Framework.Extensions;
+using ElasticSea.Framework.Util;
+using ElasticSea.Framework.Util.PropertyDrawers;
+using UnityEngine;
+
+namespace ElasticSea.Framework.Ui.Layout.Spatial
+{
+    public class SpatialLayoutGizmos : MonoBehaviour
+    {
+        [SerializeField, CustomObjectPicker(typeof(ISpatialLayout))] private Component _spatialLayout;
+        [SerializeField] private bool drawGizmos = true;
+        [SerializeField] private bool drawSelected = true;
+
+        private void OnDrawGizmosSelected()
+        {
+            if (drawGizmos && drawSelected)
+            {
+                DrawGizmos();
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (drawGizmos && !drawSelected)
+            {
+                DrawGizmos();
+            }
+        }
+
+        private void DrawGizmos()
+        {
+            var spatialLayout = (ISpatialLayout)_spatialLayout;
+            var bounds = spatialLayout.Bounds;
+            var count = spatialLayout.Count;
+
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireCube(bounds.center, bounds.size);
+
+            for (int i = 0; i < count; i++)
+            {
+                var cell = spatialLayout.GetCell(i);
+                Gizmos.matrix = transform.localToWorldMatrix * cell.cellToLocal;
+                Gizmos.color = Color.red;
+                var radius = cell.localBounds.size.FromXY().Min() / 2;
+                GizmoUtils.DrawCircle(cell.localBounds.center, Vector3.back, radius);
+            }
+        }
+    }
+}
