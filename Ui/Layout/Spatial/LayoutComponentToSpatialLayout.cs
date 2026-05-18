d394d978d1b256eeb5e1a99253e1db333b1ecef6
elasticsea <developer@elasticsea.com>
2026-05-18 20:10:00 +0200

    LayoutComponentToSpatialLayout

---
+++ Ui/Layout/Spatial/LayoutComponentToSpatialLayout.cs
@@ -0,0 +1,29 @@
+﻿using ElasticSea.Framework.Layout;
+using ElasticSea.Framework.Util.PropertyDrawers;
+using UnityEngine;
+
+namespace ElasticSea.Framework.Ui.Layout.Spatial
+{
+    public class LayoutComponentToSpatialLayout : MonoBehaviour, ISpatialLayout
+    {
+        [SerializeField, CustomObjectPicker(typeof(ILayoutComponent))] private Component _targetLayout;
+
+        private Bounds bounds
+        {
+            get
+            {
+                var layout = (ILayoutComponent)_targetLayout;
+                var rect = layout.Rect;
+                return new Bounds(rect.center, rect.size);
+            }
+        }
+
+        public int Count => 1;
+        public Bounds Bounds => bounds;
+
+        public SpatialCell GetCell(int index)
+        {
+            return new SpatialCell(Matrix4x4.identity, bounds);
+        }
+    }
+}
